b73fdfadf63301c2bdbbec5eea45c1d31ae67f37
lineis <eisele.linus@gmail.com>
2023-01-02 17:35:01 +0100

    product migrations from clara

--- core/models.py
+++ core/models.py
@@ -68,6 +68,9 @@ class Item(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     image = models.ImageField()
+    image2 = models.ImageField()
+    image3 = models.ImageField()
+    image4 = models.ImageField()
 
     def __str__(self):
         return self.title
